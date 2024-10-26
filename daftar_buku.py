787e1515ad6f7ad77197d51c53972b872a70a5fc
baxsur <bandaiz1221@gmail.com>
2024-10-26 17:39:15 +0700

    urutan daftar buku
    memperbaiki anomali pada urutan daftar buku

--- daftar_buku.py
+++ daftar_buku.py
@@ -18,7 +18,11 @@ def daftar_buku():
     }
     }
     
-    # untuk mencetak semua daftar buku yang ada pada rak_buku
+    # rak buku baru untuk membuat main keys pada rak buku menjadi angka berurutan(buku1 = 0),(buku2 = 1),dan seterusnya...
+    # dengan hal ini urutan pada buku yang ada akan tetap terpertahankan walaupun dihapus atau ditambah
+    rak_buku_baru = dict(zip(range(len(rak_buku)), rak_buku.values()))
+    
     print("\nDaftar Buku:")
-    for key, info_buku in rak_buku.items():
-        print(f"{key[-1]}. {info_buku['judul']}")
+    # untuk mencetak semua daftar buku yang ada pada rak_buku
+    for key,info_buku in rak_buku_baru.items():
+        print(f"{key+1}.{info_buku['judul']}")    
