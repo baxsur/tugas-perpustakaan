dafd9e857d7dfd2350a022e536074c068c5e67e1
baxsur <bandaiz1221@gmail.com>
2024-10-26 22:31:05 +0700

    revisi sedikit

--- daftar_buku.py
+++ daftar_buku.py
@@ -1,32 +1,33 @@
-rak_buku = {}
+import os
 
-def daftar_buku():
-
-    # daftar buku bawaan
-    rak_buku = {
-    'buku1' : {
-    'judul':'Filosofi Teras',
-    'penulis':'Hanry Manampiring',
-    'penerbit':'PT.Kompas Media Nusantara',
-    'kategori':'pengembangan diri',
-    'tahun':'2019'
+# tempat menampung semua data buku 
+rak_buku = {
+    'buku1': {
+        'judul': 'Filosofi Teras',
+        'penulis': 'Hanry Manampiring',
+        'penerbit': 'PT.Kompas Media Nusantara',
+        'kategori': 'pengembangan diri',
+        'tahun': '2019'
     },
-    'buku2' : {
-    'judul':'DUNE',
-    'penulis':'Frank Herbert',
-    'penerbit':'Chilton Books',
-    'kategori':'Novel',
-    'tahun':'1965'
+    'buku2': {
+        'judul': 'DUNE',
+        'penulis': 'Frank Herbert',
+        'penerbit': 'Chilton Books',
+        'kategori': 'Novel',
+        'tahun': '1965'
     }
-    }
-    
-    # rak buku baru untuk membuat main keys pada rak buku menjadi angka berurutan(buku1 = 0),(buku2 = 1),dan seterusnya...
-    # dengan hal ini urutan pada buku yang ada akan tetap terpertahankan walaupun dihapus atau ditambah
-    rak_buku_baru = dict(zip(range(len(rak_buku)), rak_buku.values()))
-    
-    print("\nDaftar Buku:")
-    # untuk mencetak semua daftar buku yang ada pada rak_buku
-    for key,info_buku in rak_buku_baru.items():
-        print(f"{key+1}.{info_buku['judul']}")    
+}
 
-daftar_buku()
+# fungsi yang berguna untuk mencetak daftar buku 
+def daftar_buku(rak_buku):
+    # untuk mengubah main keys pada rak_buku agar menjadi angka berurutan(buku1:0,buku2:1,dan seterusnya....)
+    rak_buku_baru = dict(zip(range(len(rak_buku)), rak_buku.values()))
+    os.system('cls')
+    print("=======================")
+    print("      Daftar Buku")
+    print("=======================")
+
+    #untuk memecah setiap main keys dan values pada rak_buku/rak_buku_baru
+    for main_keys,detail_buku in rak_buku_baru.items():
+        print(f"{main_keys+1}. {detail_buku['judul']}") #
+
--- tambah_buku.py
+++ tambah_buku.py
@@ -1,27 +1,38 @@
-def tambah_buku(rak_buku):
+from daftar_buku import daftar_buku, rak_buku
+
+def tambah_buku():
     
     # untuk menghitung jumlah buku yang ada pada rak buku,(buku1,buku2,buku3,buku4) = 4
     urutan_buku = len(rak_buku)
 
     while True:
-        # Tambah nomor buku supaya tidak ada yang namanya duplicate
+        # Tambah nomor dibelakang buku supaya tidak ada yang namanya duplikasi
         urutan_buku += 1 # jumlah buku + 1
         buku_ke = "buku" + str(urutan_buku) 
 
         # Input dari pengguna
         judul = input("Masukkan judul buku: ")
         penulis = input("Masukkan nama penulis: ")
+        penerbit = input("masukan penerbit buku: ")
+        kategori = input("masukan kategori buku: ")
         tahun = input("Masukkan tahun terbit: ")
-
         # Tambahkan buku baru hasil inputan user ke rak_buku yang berada pada file daftar buku
-        rak_buku[buku_ke] = {'judul': judul, 'penulis': penulis, 'tahun': tahun} 
+        rak_buku[buku_ke] = {
+            'judul': judul,
+            'penulis': penulis,
+            'penerbit' :penerbit,
+            'kategori': kategori,
+            'tahun': tahun} 
 
         # Tanya apakah ingin menambah buku lagi
-        lanjut = input("Tambah buku lain? (y/n): ").lower()
-        if lanjut != 'y':
+        lanjut = input("Tambah buku lain? (y/n): ").lower() #.lower untuk mengatasi ke autisan dari user bila menginput Y/T 
+        if lanjut == 'y':
+            print("")
+        else:
             break
+    
+    daftar_buku(rak_buku) # untuk menampilkan daftar buku yang baru di update admin
 
-    # Cetak seluruh isi rak_buku yang baru saja ditambkan
-    print("\nDaftar Buku:")
-    for key, info_buku in rak_buku.items():
-        print(f"{key[-1]}. {info_buku['judul']}")
+daftar_buku(rak_buku) # untuk menampilkan daftar buku bawaan 
+
+tambah_buku()
