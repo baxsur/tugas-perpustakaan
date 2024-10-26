e8d8a3c9123ad0a11bc76bc64adeeb74d9896648
baxsur <bandaiz1221@gmail.com>
2024-10-26 13:32:30 +0700

    modifikasi comment

--- daftar_buku.py
+++ daftar_buku.py
@@ -3,9 +3,11 @@ import tambah_buku
 rak_buku = {}
 
 def daftar_buku():
+
+    # daftar buku bawaan
     rak_buku = {
     'buku1' : {
-    'judul':'habit',
+    'judul':'filosofi ambatron',
     'penulis':'jarwo',
     'tahun':'2019'
     },
@@ -15,11 +17,13 @@ def daftar_buku():
     'tahun':'2024'
     }
     }
-   
+    
+    # untuk mencetak semua daftar buku yang ada pada rak_buku
     print("\nDaftar Buku:")
     for key, info_buku in rak_buku.items():
         print(f"{key[-1]}. {info_buku['judul']}")
 
+    # tinggal nambahi kondisi tambah buku dan hapus buku
     tambah_buku.tambah_buku(rak_buku)
 
 daftar_buku()
--- tambah_buku.py
+++ tambah_buku.py
@@ -1,26 +1,27 @@
 def tambah_buku(rak_buku):
     
+    # untuk menghitung jumlah buku yang ada pada rak buku,(buku1,buku2,buku3,buku4) = 4
     urutan_buku = len(rak_buku)
 
     while True:
         # Tambah nomor buku supaya tidak ada yang namanya duplicate
-        urutan_buku += 1
-        buku_ke = "buku" + str(urutan_buku)
+        urutan_buku += 1 # jumlah buku + 1
+        buku_ke = "buku" + str(urutan_buku) 
 
         # Input dari pengguna
         judul = input("Masukkan judul buku: ")
         penulis = input("Masukkan nama penulis: ")
         tahun = input("Masukkan tahun terbit: ")
 
-        # Tambahkan buku ke rak_buku
-        rak_buku[buku_ke] = {'judul': judul, 'penulis': penulis, 'tahun': tahun}
+        # Tambahkan buku baru hasil inputan user ke rak_buku yang berada pada file daftar buku
+        rak_buku[buku_ke] = {'judul': judul, 'penulis': penulis, 'tahun': tahun} 
 
         # Tanya apakah ingin menambah buku lagi
         lanjut = input("Tambah buku lain? (y/n): ").lower()
         if lanjut != 'y':
             break
 
-    # Cetak seluruh isi rak_buku
+    # Cetak seluruh isi rak_buku yang baru saja ditambkan
     print("\nDaftar Buku:")
     for key, info_buku in rak_buku.items():
         print(f"{key[-1]}. {info_buku['judul']}")
