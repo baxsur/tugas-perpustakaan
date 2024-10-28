8511a1ae15d346e13e76e7d22727b352c790e159
Hanifff <120651143+Hanifmd45@users.noreply.github.com>
2024-10-29 02:01:42 +0700

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -1,26 +1,25 @@
-import admin
-import mahasiswa
-import os
+from akunAdmin import akunAdmin
+from mahasiswa import dashboard_mahasiswa
 
 def main_menu():
-    while True:
-        os.system('cls')
-        print('''
+    print('''
 |-selamat datang di perpustakaan uty-|
 |                                    |
 |-----0.keluar dari perpustakaan-----|
 |-----1.masuk sebagai admin----------|
 |-----2.masuk sebagai mahasiswa------|''')
     
-        pilihan_akun = input("\nmasuk sebagai admin/mahasiswa? :")
+    pilihan_akun = input("\nmasuk sebagai admin/mahasiswa? :")#Memasukan Pilihan yang ada didaftar
 
-        if pilihan_akun == "1":
-            admin.admin()
-        elif pilihan_akun == "2":
-            mahasiswa.dashboard_mahasiswa()
-        elif pilihan_akun == "0":
-            print("anda telah keluar dari perpustakaan")    
-        else:
-            print("tolong masukan angka sesuai menu yang ada")
+    #Kondisi untuk memproses semua fitur
+    if pilihan_akun == "1":
+        # admin.admin()
+        akunAdmin()
+    elif pilihan_akun == "2":
+        dashboard_mahasiswa()
+    elif pilihan_akun == "0":
+        print("anda telah keluar dari perpustakaan")    
+    else:
+        print("tolong masukan angka sesuai menu yang ada")
 
 main_menu()
