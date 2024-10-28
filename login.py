f22fe92ede6f0c3c615bdb23c88d1a4db9962011
Hanifff <120651143+Hanifmd45@users.noreply.github.com>
2024-10-29 02:00:03 +0700

    Delete login.py

--- login.py
+++
@@ -1,9 +0,0 @@
-def login(user_data):
-    username_login = input("Masukkan username yang sudah ada: ")
-    password_login = input("Masukkan password yang sudah ada: ")
-
-    if username_login in user_data and user_data[username_login] == password_login:
-        print("Selamat, login berhasil!")
-        input("\nklik enter untuk lanjut")
-    else:
-        print("Username atau password salah.")
